c6a07bf3bf8eff9c50811ca424f2ae56f573ae3c
alicvz38 <83705722+alicvz38@users.noreply.github.com>
2024-11-19 18:45:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -43,6 +43,10 @@ $('.slideerHaber').owlCarousel({
 let kt = document.querySelector(".kt");
 let ktgr = document.querySelector(".altKat");
 let durum = 0;
+let a0 = document.querySelectorAll(".a0");
+let a1 = document.querySelectorAll(".a1");
+let he0 = a0.length*62;
+let he1 = a1.length*62;
 
 let arsiv = document.querySelector(".arsiv");
 let altArsiv = document.querySelector(".altArsiv");
@@ -50,24 +54,24 @@ let durum2 = 0;
 
 kt.addEventListener("click",()=>{
     if(durum == 0){
-        ktgr.style.display = "block";
-        altArsiv.style.display = "none";
+        ktgr.style.height = he0+"px";
+        altArsiv.style.height = "0px";
         durum = 1;
         durum2 = 0;
     }else{
-        ktgr.style.display = "none";
+        ktgr.style.height = "0px";
         durum = 0;
     }
 });
 
 arsiv.addEventListener("click",(e)=>{
     if(durum2 == 0){
-        altArsiv.style.display = "block";
-        ktgr.style.display = "none";
+        altArsiv.style.height = he1+"px";
+        ktgr.style.height = "0px";
         durum2 = 1;
         durum = 0;
     }else{
-        altArsiv.style.display = "none";
+        altArsiv.style.height = "0px";
         durum2 = 0;
     }
 
